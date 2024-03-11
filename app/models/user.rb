eeb4874aaa168d792083bc03c60e0bfb33da2f79
T-clow <813.danger@gmail.com>
2024-03-12 00:26:01 +0900

    add: ゲストログイン機能の実装
    - ユーザーがアカウントを作成せずにアプリケーションの機能を試せるよう、ゲストログイン機能を実装しました。
    - ゲストユーザーとしてログインすることで、すぐにアプリケーションの主要機能を体験できます。

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,17 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :full_name, presence: true
+  validates :username, presence: true, uniqueness: true
+  validates :phone_number, presence: true, numericality: { only_integer: true }, length: { minimum: 10, maximum: 15 }
+
+  def self.guest
+    find_or_create_by!(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.full_name = "ゲスト"
+      user.username = "ゲスト"
+      user.phone_number = "0000000000"
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_11_063453) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_11_151940) do
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -24,6 +24,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_11_063453) do
     t.string "phone_number"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
