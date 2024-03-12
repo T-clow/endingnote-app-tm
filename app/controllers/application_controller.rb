1212f650239111f945b1b3e1437162668e0af1a9
T-clow <813.danger@gmail.com>
2024-03-12 16:45:26 +0900

    update: テストコードの修正とユーザー編集のストロングパラメーターの更新
    - テストコードに対する修正を行いました。
    - 画像以外が更新できなくなっていましたのでユーザー編集機能におけるストロングパラメーターを更新しました。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :phone_number, :full_name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:avatar])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:email, :phone_number, :full_name, :username, :password, :password_confirmation, :current_password, :avatar])
   end
 end
