b6fa037a834ced3c810d145b0f3f142a80e20969
T-clow <813.danger@gmail.com>
2024-03-12 22:01:50 +0900

    add: RSpecによるテストの実装
    - Deviseの動作に関するテストを実装しました。これには、ログイン、ログアウト、新規登録、編集、削除のプロセスが含まれます。
    - モデルスペックを通じて、アプリケーションのデータ構造とバリデーションの正確性を検証しました。
    - リクエストスペックを用いて、アプリケーションのエンドポイントが期待通りに動作することを確認しました。
    - システムスペックにより、ユーザーインターフェースの動作をエンドツーエンドでテストし、アプリケーションがユーザーの操作に対して正しく応答することを保証しました。
    これらのテストにより、アプリケーションの信頼性と安定性が向上しました。

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,53 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe User, type: :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+RSpec.describe User, type: :model do
+  describe 'バリデーション' do
+    it '有効な属性の場合、ユーザーは有効であること' do
+      user = build(:user)
+      expect(user).to be_valid
+    end
+
+    it '電話番号がない場合、無効であること' do
+      user = build(:user, phone_number: nil)
+      user.valid?
+      expect(user.errors[:phone_number]).to include("を入力してください")
+    end
+
+    it 'ユーザ名がない場合、無効であること' do
+      user = build(:user, username: nil)
+      user.valid?
+      expect(user.errors[:username]).to include("を入力してください")
+    end
+
+    it '電話番号が数値でない場合、無効であること' do
+      user = build(:user, phone_number: 'abcdefghij')
+      user.valid?
+      expect(user.errors[:phone_number]).to include("は数値で入力してください")
+    end
+
+    it '名前がない場合、無効であること' do
+      user = build(:user, full_name: nil)
+      user.valid?
+      expect(user.errors[:full_name]).to include("を入力してください")
+    end
+  end
+
+  describe '.guest' do
+    it 'ゲストユーザーを作成または取得できること' do
+      guest_user = User.guest
+      expect(guest_user.email).to eq('guest@example.com')
+    end
+  end
+
+  describe '#guest?' do
+    it 'ゲストユーザーの場合、trueを返すこと' do
+      guest_user = User.guest
+      expect(guest_user.guest?).to be true
+    end
+
+    it 'ゲストユーザーでない場合、falseを返すこと' do
+      non_guest_user = create(:user, email: 'non_guest@example.com')
+      expect(non_guest_user.guest?).to be false
+    end
+  end
+end
---
+++ spec/requests/user_sessions_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe "UserSessions", type: :request do
+  let(:user) { FactoryBot.create(:user) }
+
+  describe "ログイン" do
+    it "ログインできること" do
+      post user_session_path, params: { user: { email: user.email, password: user.password } }
+      expect(response).to redirect_to('/')
+    end
+  end
+
+  describe "ログアウト" do
+    it "ログアウトできること" do
+      sign_in user
+      delete destroy_user_session_path
+      expect(response).to redirect_to('/')
+    end
+  end
+end
