ca67e8598c585396965c5062d611346f028b5b73
T-clow <813.danger@gmail.com>
2024-03-12 13:28:36 +0900

    fix: コントローラのインデント修正
    - RuboCopの指摘に従い、コントローラ内のインデントを修正しました。

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,11 +1,11 @@
 class RegistrationsController < Devise::RegistrationsController
-    before_action :check_guest_user, only: [:edit, :update]
-  
-    private
-  
-    def check_guest_user
-      if resource.email == 'guest@example.com'
-        redirect_to "/", alert: 'ゲストユーザーのプロフィールは編集できません。'
-      end
+  before_action :check_guest_user, only: [:edit, :update]
+
+  private
+
+  def check_guest_user
+    if resource.email == 'guest@example.com'
+      redirect_to "/", alert: 'ゲストユーザーのプロフィールは編集できません。'
     end
   end
+end
