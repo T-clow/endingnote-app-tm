337007d59860003d035fba1b9097b7d6ea955b1d
T-clow <813.danger@gmail.com>
2024-03-13 11:54:05 +0900

    refactor: テスト構文の修正
    - RSpecテストの構文を見直し、より読みやすく理解しやすい形に修正しました。

--- spec/requests/user_sessions_spec.rb
+++ spec/requests/user_sessions_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "UserSessions", type: :request do
-  let(:user) { FactoryBot.create(:user) }
+  let(:user) { create(:user) }
 
   describe "ログイン" do
     it "ログインできること" do
--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :system do
-  let(:user) { FactoryBot.create(:user) }
+  let(:user) { create(:user) }
 
   before do
     driven_by(:rack_test)
