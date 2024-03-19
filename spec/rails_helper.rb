ad3d9cbb420ac9978ccbbf2d7eb989ea39c77cb0
T-clow <813.danger@gmail.com>
2024-03-19 14:42:29 +0900

    update: CI用のCapybaraテスト設定をrails_helperを変更3
    - CI環境でのSystem Spec実行に必要なCapybaraの設定をローカルと同じ設定にしました。
    - 再度挙動を確認するためのコミットです。

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,7 +7,7 @@ require 'capybara/rails'
 require 'selenium-webdriver'
 
 if ENV['CI'] == 'true'
-  Capybara.register_driver :selenium_remote_chrome_ci do |app|
+  Capybara.register_driver :selenium_remote_chrome do |app|
     options = Selenium::WebDriver::Chrome::Options.new
     options.add_argument('--headless')
     options.add_argument('--disable-gpu')
@@ -17,7 +17,7 @@ if ENV['CI'] == 'true'
     Capybara::Selenium::Driver.new(app,
       browser: :remote,
       url: "http://localhost:4444/wd/hub",
-      capabilities: options)
+      capabilities: [options])
   end
   Capybara.javascript_driver = :selenium_remote_chrome
 else
