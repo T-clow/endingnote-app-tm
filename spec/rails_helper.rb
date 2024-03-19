3172e11a58d361e7898f02975c02ab65036bc235
T-clow <813.danger@gmail.com>
2024-03-19 14:00:02 +0900

    update: CI用のCapybaraテスト設定をrails_helperに追加
    - CI環境でのSystem Spec実行に必要なCapybaraの設定をrails_helperに記載しました。
    - この変更により、GitHub Actionsを使用したCIプロセスでのSystem Specの挙動確認が可能になります。
    - 再度挙動を確認するためのコミットです。

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,20 +6,35 @@ require 'rspec/rails'
 require 'capybara/rails'
 require 'selenium-webdriver'
 
-Capybara.register_driver :selenium_remote_chrome do |app|
-  options = Selenium::WebDriver::Chrome::Options.new
-  options.add_argument('--headless')
-  options.add_argument('--disable-gpu')
-  options.add_argument('--no-sandbox')
-  options.add_argument('--window-size=1400,1400')
+if ENV['CI'] == 'true'
+  Capybara.register_driver :selenium_remote_chrome_ci do |app|
+    options = Selenium::WebDriver::Chrome::Options.new
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--window-size=1400,1400')
 
-  Capybara::Selenium::Driver.new(app,
-    browser: :remote,
-    url: "http://chrome:4444/wd/hub",
-    capabilities: options)
+    Capybara::Selenium::Driver.new(app,
+      browser: :remote,
+      url: "http://chrome:4444/wd/hub",
+      capabilities: options)
+  end
+  Capybara.javascript_driver = :selenium_remote_chrome_ci
+else
+  Capybara.register_driver :selenium_remote_chrome do |app|
+    options = Selenium::WebDriver::Chrome::Options.new
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--window-size=1400,1400')
+
+    Capybara::Selenium::Driver.new(app,
+      browser: :remote,
+      url: "http://chrome:4444/wd/hub",
+      capabilities: options)
+  end
+  Capybara.javascript_driver = :selenium_remote_chrome
 end
-
-Capybara.javascript_driver = :selenium_remote_chrome
 
 Capybara.server_host = '0.0.0.0'
 Capybara.server_port = 3001
