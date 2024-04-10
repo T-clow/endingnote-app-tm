d9a158b3de9a31879293720321c5f523ffc32313
T-clow <813.danger@gmail.com>
2024-04-10 16:52:14 +0900

    fix: トグルメニューと削除確認メッセージのJavaScriptの干渉を解消
    - トグルメニューと削除確認メッセージのJavaScriptが干渉していることを発見しました。
    - 開閉の動きをjQueryの使用をやめて手書きのJavaScriptで対応しました。2つの動作を確認しました。
    - また、JavaScriptで設定していた動画の時間制限のメッセージが反応しなくなったため、一旦削除しました。

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,9 +1,27 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-//= require jquery3
-//= require popper
-//= require bootstrap
 import "@hotwired/turbo-rails"
 import "controllers"
 import { Turbo } from "@hotwired/turbo-rails"
 Turbo.start()
 
+document.addEventListener("turbo:load", function() {
+    resetNavbar();
+    setupNavbarToggle();
+});
+
+function resetNavbar() {
+    const navbarCollapse = document.querySelector('.navbar-collapse');
+    if (navbarCollapse.classList.contains('show')) {
+        navbarCollapse.classList.remove('show');
+    }
+}
+
+function setupNavbarToggle() {
+    const navbarToggler = document.querySelector('.navbar-toggler');
+    navbarToggler.removeEventListener('click', toggleNavbar);
+    navbarToggler.addEventListener('click', toggleNavbar);
+}
+
+function toggleNavbar() {
+    const navbarCollapse = document.querySelector('.navbar-collapse');
+    navbarCollapse.classList.toggle('show');
+}
