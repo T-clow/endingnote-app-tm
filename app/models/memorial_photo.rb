fa280e1e03e675b3f9054edef24aa34412099f7e
T-clow <813.danger@gmail.com>
2024-04-18 15:00:39 +0900

    add: 画像ファイルの種類とコメントの文字数制限のバリデーションの設定をしました
    - インデックスページのカードの配置サイズをCSSで調整しました。

--- app/models/memorial_photo.rb
+++ app/models/memorial_photo.rb
@@ -3,7 +3,9 @@ class MemorialPhoto < ApplicationRecord
   has_one_attached :photo
   validates :photo, presence: true
   validates :visible_to, inclusion: { in: ['family'] }
+  validates :comment, length: { maximum: 100 }
   validate :photo_count_within_limit, on: :create
+  validate :acceptable_image
 
   after_initialize :set_default_visible_to
 
@@ -16,6 +18,18 @@ class MemorialPhoto < ApplicationRecord
   def photo_count_within_limit
     if user.memorial_photos.count >= 3
       errors.add(:base, "画像は3枚まで投稿可能です")
+    end
+  end
+
+  def acceptable_image
+    return unless photo.attached?
+
+    unless photo.content_type.in?(%w(image/jpeg image/png))
+      errors.add(:photo, 'はJPEGまたはPNG形式である必要があります')
+    end
+
+    if photo.byte_size > 30.megabytes
+      errors.add(:photo, 'のサイズは30MB以下である必要があります')
     end
   end
 end
