0ca430e39f5ca9f9091991cf2d254fb4cd4c74a2
T-clow <813.danger@gmail.com>
2024-05-09 13:17:50 +0900

    update: インデックスページにchart.jsを使用したサンプルのグラフの表示に成功しました
    - Herokuでの動作確認のためコミットしました。

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,9 +1,7 @@
-import "@hotwired/turbo-rails"
-import "controllers"
-import "map";
-import { Turbo } from "@hotwired/turbo-rails"
-
-Turbo.start()
+import "@hotwired/turbo-rails";
+import "controllers";
+import { Turbo } from "@hotwired/turbo-rails";
+Turbo.start();
 
 document.addEventListener("turbo:load", function() {
     resetNavbar();
@@ -12,33 +10,41 @@ document.addEventListener("turbo:load", function() {
 
 function resetNavbar() {
     const navbarCollapse = document.querySelector('.navbar-collapse');
-    if (navbarCollapse.classList.contains('show')) {
+    if (navbarCollapse && navbarCollapse.classList.contains('show')) {
         navbarCollapse.classList.remove('show');
     }
 }
 
 function setupNavbarToggle() {
     const navbarToggler = document.querySelector('.navbar-toggler');
-    navbarToggler.removeEventListener('click', toggleNavbar);
-    navbarToggler.addEventListener('click', toggleNavbar);
+    if (navbarToggler) {
+        navbarToggler.removeEventListener('click', toggleNavbar);
+        navbarToggler.addEventListener('click', toggleNavbar);
+    }
 }
 
 function toggleNavbar() {
     const navbarCollapse = document.querySelector('.navbar-collapse');
-    navbarCollapse.classList.toggle('show');
+    if (navbarCollapse) {
+        navbarCollapse.classList.toggle('show');
+    }
 }
 
 import { createApp } from "vue";
-import * as Controllers from "controllers/application";
 
 document.addEventListener("turbo:load", function() {
-  const App = createApp({
-    data() {
-      return {
-        message: 'Hello Vue'
+  const vueAppElement = document.querySelector("#vue-app");
+  if (vueAppElement) {
+    const App = createApp({
+      data() {
+        return {
+          message: 'Hello Vue'
+        }
       }
-    }
-  });
+    });
 
-  App.mount("#vue-app");
+    App.mount(vueAppElement);
+  }
 });
+
+import "charts/bar_chart";
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,5 +1,3 @@
-# Pin npm packages by running ./bin/importmap
-
 pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
@@ -10,3 +8,8 @@ pin "@popperjs/core", to: "popper.js", preload: true
 pin "video_validation", to: "video_validation.js"
 pin "map"
 pin "vue", to: "https://cdn.jsdelivr.net/npm/vue@3.2.41/dist/vue.esm-browser.js"
+pin "chart.js" # @4.4.2
+pin "@kurkle/color", to: "@kurkle--color.js" # @0.3.2
+pin "charts/bar_chart", to: "charts/bar_chart.js"
+pin "chart.js", to: "https://ga.jspm.io/npm:chart.js@4.3.0/dist/chart.js"
+
