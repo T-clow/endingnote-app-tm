2f6db5a0309508e2a423f4ba227adbdde215b95f
T-clow <813.danger@gmail.com>
2024-05-14 16:25:38 +0900

    update: import.mapを修正し、Vue.jsのリンクを本番と開発環境で切り替える仕様に変更しました

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,10 +6,11 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
 pin "video_validation", to: "video_validation.js"
-pin "map"
-pin "vue", to: "https://cdn.jsdelivr.net/npm/vue@3.2.41/dist/vue.esm-browser.js"
-pin "chart.js" # @4.4.2
-pin "@kurkle/color", to: "@kurkle--color.js" # @0.3.2
+if Rails.env.production?
+  pin "vue", to: "https://cdn.jsdelivr.net/npm/vue@3.2.41/dist/vue.global.prod.js", preload: true
+else
+  pin "vue", to: "https://cdn.jsdelivr.net/npm/vue@3.2.41/dist/vue.esm-browser.js", preload: true
+end
+pin "chart.js", to: "https://cdn.jsdelivr.net/npm/chart.js@4.3.0/dist/chart.js", preload: true
+pin "@kurkle/color", to: "https://cdn.jsdelivr.net/npm/@kurkle/color@0.3.2/dist/color.esm.js", preload: true
 pin "charts/bar_chart", to: "charts/bar_chart.js"
-pin "chart.js", to: "https://ga.jspm.io/npm:chart.js@4.3.0/dist/chart.js"
-
