21a183080942ed3068ba1761ff0bfb5885a6147c
T-clow <813.danger@gmail.com>
2024-05-14 16:48:41 +0900

    fix: エラーでグラフが表示できなくなったため、一旦元に戻しました。

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,3 +1,5 @@
+# config/importmap.rb
+
 pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
@@ -6,11 +8,7 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
 pin "video_validation", to: "video_validation.js"
-if Rails.env.production?
-  pin "vue", to: "https://cdn.jsdelivr.net/npm/vue@3.2.41/dist/vue.global.prod.js", preload: true
-else
-  pin "vue", to: "https://cdn.jsdelivr.net/npm/vue@3.2.41/dist/vue.esm-browser.js", preload: true
-end
+pin "vue", to: "https://cdn.jsdelivr.net/npm/vue@3.2.41/dist/vue.esm-browser.js", preload: true
 pin "chart.js", to: "https://cdn.jsdelivr.net/npm/chart.js@4.3.0/dist/chart.js", preload: true
 pin "@kurkle/color", to: "https://cdn.jsdelivr.net/npm/@kurkle/color@0.3.2/dist/color.esm.js", preload: true
 pin "charts/bar_chart", to: "charts/bar_chart.js"
