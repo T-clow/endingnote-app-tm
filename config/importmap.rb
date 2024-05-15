9e9d329b7d5070994b9510dec9fd331118e820b9
T-clow <813.danger@gmail.com>
2024-05-15 14:30:51 +0900

    fix: マップの挙動がおかしくなったため、import.mapを元に戻しました。

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,5 +1,3 @@
-# config/importmap.rb
-
 pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
@@ -8,7 +6,9 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
 pin "video_validation", to: "video_validation.js"
+pin "map"
 pin "vue", to: "https://cdn.jsdelivr.net/npm/vue@3.2.41/dist/vue.esm-browser.js", preload: true
-pin "chart.js", to: "https://cdn.jsdelivr.net/npm/chart.js@4.3.0/dist/chart.js", preload: true
-pin "@kurkle/color", to: "https://cdn.jsdelivr.net/npm/@kurkle/color@0.3.2/dist/color.esm.js", preload: true
+pin "chart.js"
+pin "@kurkle/color", to: "@kurkle--color.js"
 pin "charts/bar_chart", to: "charts/bar_chart.js"
+pin "chart.js", to: "https://ga.jspm.io/npm:chart.js@4.3.0/dist/chart.js"
