88dab237e7bd02f1a448e5cdd34dedc27c9c89e2
T-clow <813.danger@gmail.com>
2024-05-24 15:39:56 +0900

    update: 保険金額計算の部分をブートストラップで装飾しました
    - HTMLのテキストを変更したため、それに対応するシステムテストを修正しました。
    - 保険契約がない年齢を入力した場合に警告文を表示するテストケースを追加しました。

--- spec/system/insurance_amount_calculations_spec.rb
+++ spec/system/insurance_amount_calculations_spec.rb
@@ -13,28 +13,35 @@ RSpec.describe "InsuranceAmountCalculations", type: :system do
   end
 
   describe "保険金額計算" do
-    it "正しい年齢を入力して保険金額の合計を表示する" do
+    it "正しい年齢を入力して保険金額の合計を表示すること" do
       visit user_insurance_graphs_path(user)
-      fill_in '年齢を入力してください', with: 65
+      fill_in '指定年齢で保険金額計算ができます', with: 65
       click_button '計算'
-      expect(page).to have_content("#{user.username}様が65歳の時にお亡くなりになると1500万円家族に遺せます")
+      expect(page).to have_content("#{user.username}様が65歳時点でお亡くなりになると1500万円家族に遺せます")
     end
 
-    it "ユーザーの年齢より低い年齢を入力した場合にエラーメッセージを表示する" do
+    it "保険契約がない年齢年齢を入力した場合警告文をを表示すること" do
       visit user_insurance_graphs_path(user)
-      fill_in '年齢を入力してください', with: 25
+      fill_in '指定年齢で保険金額計算ができます', with: 99
+      click_button '計算'
+      expect(page).to have_content("#{user.username}様が99歳時点でお亡くなりになられても遺せるお金はありません")
+    end
+
+    it "ユーザーの年齢より低い年齢を入力した場合にエラーメッセージを表示すること" do
+      visit user_insurance_graphs_path(user)
+      fill_in '指定年齢で保険金額計算ができます', with: 25
       click_button '計算'
       expect(page).to have_content("年齢は#{user.birthday.age}歳以上にしてください。")
     end
 
-    it "100歳より高い年齢を入力した場合にエラーメッセージを表示する" do
+    it "100歳より高い年齢を入力した場合にエラーメッセージを表示すること" do
       visit user_insurance_graphs_path(user)
-      fill_in '年齢を入力してください', with: 101
+      fill_in '指定年齢で保険金額計算ができます', with: 101
       click_button '計算'
       expect(page).to have_content("年齢は100歳以下で入力してください。")
     end
 
-    it "年齢を入力しなかった場合にエラーメッセージを表示する" do
+    it "年齢を入力しなかった場合にエラーメッセージを表示すること" do
       visit user_insurance_graphs_path(user)
       click_button '計算'
       expect(page).to have_content("年齢を入力してください。")
